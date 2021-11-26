d2e111190d9399bedd4b2d311fba025bb755ead8
Giuseppe Silano <giuseppe.silano@fel.cvut.cz>
2021-11-26 21:07:12 +0100

    Add state machine for bird diverter installation task

--- scripts/drone_manager_replanner_uav1.py
+++ scripts/drone_manager_replanner_uav1.py
@@ -39,7 +39,9 @@ class DroneManager:
 
         ## | --------------------- load parameters -------------------- |
 
-        self.land_duration_ = rospy.get_param('~land_duration')
+        self.land_duration_      = rospy.get_param('~land_duration')
+        self.refilling_duration_ = rospy.get_param('~refilling_time')
+        self.delay_duration_     = rospy.get_param('~delay')
 
         self.goto_x_       = rospy.get_param('~goto_destination/x')
         self.goto_y_       = rospy.get_param('~goto_destination/y')
@@ -50,34 +52,35 @@ class DroneManager:
 
         self.reference_frame_ = rospy.get_param('~reference_frame')
 
-        self.trajectory_file_ = rospy.get_param('~trajectory_file')
+        self.trajectory_file_1_ = rospy.get_param('~trajectory_file_1')
 
         # TODO: !!! check if the params have been loaded successfully
 
         ## | ---------------- load trajectory from file --------------- |
 
-        # the trajectory object for publishing
-        self.trajectory_ = TrajectoryReferenceSrvRequest()
+        # the trajectory object for publishing -- First Subtrajectory
+        self.trajectory_1_ = TrajectoryReferenceSrvRequest()
 
-        self.trajectory_.trajectory.header.frame_id = self.reference_frame_
-        self.trajectory_.trajectory.fly_now = False # cause we will call goto
-        self.trajectory_.trajectory.dt = 1.0/self.trajectory_sampling_rate_
-        self.trajectory_.trajectory.use_heading = True
+        self.trajectory_1_.trajectory.header.frame_id = self.reference_frame_
+        self.trajectory_1_.trajectory.fly_now = False # cause we will call goto
+        self.trajectory_1_.trajectory.dt = 1.0/self.trajectory_sampling_rate_
+        self.trajectory_1_.trajectory.use_heading = True
 
-        with open(self.trajectory_file_, newline='') as csvfile:
+        # Read the first subjtrajectory
+        with open(self.trajectory_file_1_, newline='') as csvfile:
 
-            reader = csv.reader(csvfile, delimiter=' ')
+            reader = csv.reader(csvfile, delimiter=',')
 
             for row in reader:
 
-                point = Reference()
+                point_1 = Reference()
 
-                point.position.x = float(row[0])
-                point.position.y = float(row[1])
-                point.position.z = float(row[2])
-                point.heading    = float(row[3])
+                point_1.position.x = float(row[0])
+                point_1.position.y = float(row[1])
+                point_1.position.z = float(row[2])
+                point_1.heading    = float(row[3])
 
-                self.trajectory_.trajectory.points.append(point)
+                self.trajectory_1_.trajectory.points.append(point_1)
 
         ## | --------------------- service servers -------------------- |
 
@@ -164,56 +167,44 @@ class DroneManager:
 
         rospy.loginfo_once("timerMain() spinning")
 
-        # --------------------------------------------------------------
-        # |        the state machine should be placed in here  \/      |
-        # --------------------------------------------------------------
+        # ----------------------------------------------------------------------------
+        # |        the sequential functional chart should be placed in here  \/      |
+        # ---------------------------------------------------------------------------
 
         while not self.freeToCommand():
             rospy.sleep(0.1)
             rospy.loginfo('waiting for the UAV to be ready')
 
-        self.setTrajectory(self.trajectory_)
+        self.setTrajectory(self.trajectory_1_)
 
-        self.gotoStart()
-
-        while not self.freeToCommand():
-            rospy.sleep(0.1)
-            rospy.loginfo('waiting for the UAV to stop')
-
-        self.startTrajectoryTracking()
+        self.gotoStart() # The UAV go to the starting point
 
         while not self.freeToCommand():
             rospy.sleep(0.1)
             rospy.loginfo('waiting for the UAV to stop')
 
-        self.gotoXYZH(self.goto_x_, self.goto_y_, self.goto_z_, self.goto_heading_)
+        self.startTrajectoryTracking() # The UAV start tracking the trajectory
 
         while not self.freeToCommand():
             rospy.sleep(0.1)
             rospy.loginfo('waiting for the UAV to stop')
 
-        self.land()
+        self.land() # The UAV reached the first refilling station. The UAV can land
 
         rospy.loginfo('sleeping for additional {} s'.format(self.land_duration_))
-        rospy.sleep(self.land_duration_)
+        rospy.sleep(self.land_duration_) # Waiting for the time to land
 
         while not self.landed():
             rospy.sleep(0.1)
             rospy.loginfo('waiting for landing')
-
-        self.takeoff()
-
-        while not self.freeToCommand():
-            rospy.sleep(0.1)
-            rospy.loginfo('waiting for takeoff')
-
+                     
         self.finished_ = True
 
         rospy.loginfo('FINISHED')
 
-        # --------------------------------------------------------------
-        # |        the state machine should be placed in here  /\      |
-        # --------------------------------------------------------------
+        # ----------------------------------------------------------------------------
+        # |        the sequential functional chart should be placed in here  /\      |
+        # ---------------------------------------------------------------------------
 
 # #} end of timerMain()
 
@@ -317,7 +308,7 @@ class DroneManager:
         point.position.x       = x
         point.position.y       = y
         point.position.z       = z
-        point.heading = heading
+        point.heading          = heading
 
         path.path.points.append(point)
 
